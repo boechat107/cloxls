1165459e2e5fc66fe24c0ba546406b5669badf4b
Andre Ambrosio Boechat <boechat107@gmail.com>
2012-11-12 21:41:58 -0200

    Macro to manipulate a sheet and to write its contents to file

--- src/cloxls/writer.clj
+++ src/cloxls/writer.clj
@@ -28,13 +28,21 @@
              (str "Problem to create a xls file: " (.getMessage e))))))
 
 
+(def ^{:dynamic true
+       :doc "This variable is bound to a sheet when the WITH-SHEET macro is used."}
+       *sheet* nil)
+
+
 (defn create-sheet!
-  "Creates a sheet with the given name or with a default name. Side effects only."
+  "Creates a sheet with the given name or with a default name and points the variable
+   *sheet* to the new sheet. Side effects only." 
   ([wb] 
    (let [n (inc (.getNumberOfSheets wb))]
      (create-sheet! wb (str "Sheet " n) n)))
   ([wb sheet-name idx]
-   (.createSheet wb sheet-name idx)))
+   (let [*sheet* (.createSheet wb sheet-name idx)]
+     *sheet*)))
+
 
 (defn add-label!
   "Add a text label to a specific cell."
@@ -48,13 +56,25 @@
 
 (defn add-cell-data!
   "If the data is a string, a label is created. Otherwise, a number is created."
-  [sheet row col data]
-  (let [dtype (if (number? data)
-                (jxl.write.Number. row col data)
-                (Label. row col data))]
-    (.addCell sheet dtype)))
+  ([row col data] (add-cell-data! *sheet* row col data))
+  ([sheet row col data]
+   (let [dtype (if (number? data)
+                 (jxl.write.Number. row col data)
+                 (Label. row col data))]
+     (.addCell sheet dtype))))
 
-(defn write-data!
+
+(defmacro with-sheet
+  "Binds the variable *sheet* to a sheet with the given sheet-id of the workbook wb,
+   exposes it to the body and write the modifications to file."
+  [wb sheet-id & body]
+  `(let [wb# ~wb]
+     (binding [*sheet* (.getSheet wb# ~sheet-id)]
+       ~@body
+       (.write wb#))))
+
+
+(defn close-workbook!
   [wb]
   (doto wb (.write) (.close)))
 
--- test/cloxls/writer_test.clj
+++ test/cloxls/writer_test.clj
@@ -2,13 +2,23 @@
     (:require
       [cloxls.writer :as w]))
 
-(let [wb (w/create-workbook "test.xls")
-      sheet (w/create-sheet! wb)]
-  (doto sheet
-        (w/add-cell-data! 0 0 "Company")
-        (w/add-cell-data! 0 1 "Number of Employees")
-        (w/add-cell-data! 1 0 "Dog show")
-        (w/add-cell-data! 1 1 4)
-        )
-  (w/write-data! wb)
+;(let [wb (w/create-workbook "test.xls")
+;      sheet (w/create-sheet! wb)]
+;  (doto sheet
+;        (w/add-cell-data! 0 0 "Company")
+;        (w/add-cell-data! 0 1 "Number of Employees")
+;        (w/add-cell-data! 1 0 "Dog show")
+;        (w/add-cell-data! 1 1 4)
+;        )
+;  (w/close-workbook! wb)
+;  )
+
+(with-open [wb (w/create-workbook "test.xls")]
+  (w/create-sheet! wb)
+  (w/with-sheet wb 0
+    (w/add-cell-data! 0 0 "Company")
+    (w/add-cell-data! 0 1 "Number of Employees")
+    (w/add-cell-data! 1 0 "Dog show")
+    (w/add-cell-data! 1 1 10)
+    )
   )
