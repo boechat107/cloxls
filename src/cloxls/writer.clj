e2e65fe55283d4e05ed8076ef54a4d12e7ad1cd8
Andre Ambrosio Boechat <boechat107@gmail.com>
2012-11-14 09:57:55 -0200

    Formula cell creation

--- src/cloxls/writer.clj
+++ src/cloxls/writer.clj
@@ -13,8 +13,6 @@
       )
     )
 
-;; TODO: binding macro for a sheet or spreadsheet.
-
 (defn create-workbook
   "Creates and returns the high level abstraction of the spreadsheet, a WritableWorkbook
    object. The default Locale is EN." 
@@ -32,36 +30,61 @@
        :doc "This variable is bound to a sheet when the WITH-SHEET macro is used."}
        *sheet* nil)
 
+;; ============== Sheet contents ==============================
 
 (defn create-sheet!
-  "Creates a sheet with the given name or with a default name and points the variable
-   *sheet* to the new sheet. Side effects only." 
+  "Creates a sheet with the given name or with a default name. Side effects only." 
   ([wb] 
    (let [n (inc (.getNumberOfSheets wb))]
      (create-sheet! wb (str "Sheet " n) n)))
   ([wb sheet-name idx]
-   (let [*sheet* (.createSheet wb sheet-name idx)]
-     *sheet*)))
+   (.createSheet wb sheet-name idx)))
 
 
-(defn add-label!
-  "Add a text label to a specific cell."
-  [sheet row col text]
-  (.addCell sheet (Label. row col text)))
+;(defn add-label!
+;  "Add a text label to a specific cell."
+;  [sheet row col text]
+;  (.addCell sheet (Label. row col text)))
+;
+;(defn add-number!
+;  "Add a number to a specific cell."
+;  [sheet row col num]
+;  (.addCell sheet (jxl.write.Number. row col num)))
 
-(defn add-number!
-  "Add a number to a specific cell."
-  [sheet row col num]
-  (.addCell sheet (jxl.write.Number. row col num)))
 
 (defn add-cell-data!
-  "If the data is a string, a label is created. Otherwise, a number is created."
+  "Adds data to a specific cell. If the data is a string, a label is created. Otherwise, a
+   number is created. A formula is created using a string where the first character is =."
   ([row col data] (add-cell-data! *sheet* row col data))
   ([sheet row col data]
-   (let [dtype (if (number? data)
-                 (jxl.write.Number. row col data)
-                 (Label. row col data))]
+   (let [dtype (cond
+                 (number? data) (jxl.write.Number. col row data)
+                 (and (string? data) (= \= (get data 0))) (Formula. col row (subs data 1))
+                 :else (Label. col row data))]
      (.addCell sheet dtype))))
+
+
+(defn- coll-idx-data
+  "Returns a new collection composed of tuples (idx, element), where idx is the index of
+   the element in the original collection."
+  [coll]
+  (partition 2 (interleave (range (count coll)) coll)))
+
+
+(defn add-line-data!
+  "Adds data from a collection to a specific sheet's line, the row number."
+  ([row coll] (add-line-data! *sheet* row coll))
+  ([sheet row coll]
+   (doseq [[col data] (coll-idx-data coll)]
+     (add-cell-data! sheet row col data))))
+
+
+(defn add-2d-data!
+  "Adds data to a sheet as a matrix, starting from line 0 and column 0."
+  ([mat] (add-2d-data! *sheet* mat))
+  ([sheet mat]
+   (doseq [[row line] (coll-idx-data mat)]
+     (add-line-data! sheet row line))))
 
 
 (defmacro with-sheet
@@ -77,5 +100,3 @@
 (defn close-workbook!
   [wb]
   (doto wb (.write) (.close)))
-
-
--- test/cloxls/writer_test.clj
+++ test/cloxls/writer_test.clj
@@ -22,3 +22,20 @@
     (w/add-cell-data! 1 1 10)
     )
   )
+
+(with-open [wb (w/create-workbook "test2.xls")]
+  (w/create-sheet! wb)
+  (w/with-sheet wb 0
+    (w/add-line-data! 0 ["Company" "Number of Employees"])
+    (w/add-line-data! 1 ["Dog show" 15])))
+
+(with-open [wb (w/create-workbook "test3.xls")]
+  (w/create-sheet! wb)
+  (w/with-sheet wb 0
+    (w/add-2d-data! [["Company" "Number of Employees"]
+                     ["Dog show" 20]
+                     ["Lazy comp" 5]
+                     ["Total" "=SUM(B2,B3)"]
+                     ])))
+
+
