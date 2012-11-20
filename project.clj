8601d36312aacf74b7248b22b205a0b3fd2ed817
Andre Ambrosio Boechat <boechat107@gmail.com>
2012-11-20 19:30:52 -0200

    First functional version; starting another one

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject cloxls "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cloxls "0.1.0"
+  :description "Clojure library to write and read XLS files."
+  :url "https://github.com/boechat107/cloxls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [jxl "2.6.12"]]
+                 [jxl "2.6.12"]
+                 [poi "3.8"]]
   :resource-paths ["resources"]
   )
