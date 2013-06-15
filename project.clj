5fff475b1ea927c8870c180c1e1b5a2196596340
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-06-15 12:37:12 -0300

    Removed unused options; poi update

--- project.clj
+++ project.clj
@@ -4,12 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.apache.poi/poi "3.8"]]
-  :profiles {:dev {:resource-paths ["resources"] 
-                   :dependencies [[markdown-clj "0.9.10"]]}}
-;  :dev-dependencies [[lein-marginalia "0.7.1"]]
-;  :resource-paths ["resources"]
-;  :html5-docs-ns-includes #"^cloxls\..*"
-;  :html5-docs-ns-excludes #".*jxl.*"
-;  :html5-docs-repository-url "https://github.com/boechat107/cloxls/blob/master"
+                 [org.apache.poi/poi "3.9"]]
+  :profiles {:dev {:resource-paths ["resources"]}}
   )
